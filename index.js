c179e2e49e3fcef6633cb9f03550cb3cd6626466
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-06-15 00:01:10 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // code your solution here
+/*function saturdayFun(fun = "roller-skate") {
+  return `This Saturday, I want to ${fun}!`;
+}
+saturdayFun();
+
+function mondayWork(work = "go to the office") {
+  return `This Monday, I will ${work}.`;
+}
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    // outer function return
+    return `You are ${flair}${adjective}${flair}!`; //inner function return
+  };
+}*/
+
+
+
+function saturdayFun (fun="sleep") {
+console.log (`This Saturday, I want to ${fun}!`);
+}
+saturdayFun();
+
+function saturdayFun (fun='roller-skate') {
+  return (`This Saturday, I want to ${fun}!`);
+  }
+  
+
+  function mondayWork (work='go to the office'){
+    return (`This Monday, I will ${work}.`)
+  }
+  function wrapAdjective(result= '*'){
+    return function (adjective = "a hard worker") {
+    return (`You are ${result}${adjective}${result}!`)
+  }
+}
+  
+  
+
+
+
+
+
+
